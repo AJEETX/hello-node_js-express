4cfe10dbe137cc77ff66f9fce8c20c3505c11a99
ajeet kumar <ajeetx@email.com>
2019-03-25 16:40:43 +1100

    jj

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.links({'ENDPOINTS': ['/swagger']});
+  res.links({ENDPOINTS: ['/swagger']});
   res.send('hello, readify');
 });
 
